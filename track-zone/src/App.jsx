fec0976b88e6194710858ec393a1c959462b22de
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-07-05 22:40:39 +0600

    time zone created

--- track-zone/src/App.jsx
+++ track-zone/src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import LocalClock from "./components/localClock/LocalClock.jsx";
 import ClockList from "./components/clockList/ClockList.jsx";
+import useClock from "./hooks/useClock.jsx";
 
 const App = () => {
+    const {clock: local} = useClock()
+    const {clock: est} = useClock("EST")
+    const {clock: pst} = useClock("PST")
+    const {clock: pakistan} = useClock( "UTC", 5*60 )
+
+    console.log('local utc', local.date)
+    console.log('EST', est.date)
+    console.log('PST', pst.date)
+    console.log('Pakistan', pakistan.date)
     return (
         <div>
             <LocalClock/>
--- track-zone/src/hooks/useClock.jsx
+++ track-zone/src/hooks/useClock.jsx
@@ -1,10 +1,58 @@
+import {useEffect, useState} from "react";
+import {addMinutes} from "date-fns";
 
-const UseClock = () => {
-    return (
-        <div>
 
-        </div>
-    );
+const init = {
+    id: '',
+    title: '',
+    timezone: {
+      type: '',
+      offset: '',
+    },
+    date_utc: null,
+    date: null,
+
+}
+
+    const TIMEZONE_OFFSET = {
+        PST: -7 * 60,
+        EST: -4 * 60,
+}
+
+const UseClock = ( timezone, offset = 0 ) => {
+    const [state, setState] = useState({...init})
+    const [utc, setUTC] = useState(null)
+
+    useEffect(() => {
+        let d = new Date();
+        const localOffset = d.getTimezoneOffset();
+        d = addMinutes(d, localOffset);
+        setUTC(d);
+    }, []);
+        useEffect(()=>{
+
+            if (utc !== null && timezone){
+                if(TIMEZONE_OFFSET === 'PST' || TIMEZONE_OFFSET === 'EST'){
+                    offset = TIMEZONE_OFFSET[timezone]
+                }
+                const newUtc = addMinutes(utc, offset)
+                    setState({
+                        ...state,
+                        date_utc: utc,
+                        date: newUtc
+                    })
+            }else{
+                setState({
+                   ...state,
+                    date_utc: utc,
+                    date: utc,
+                })
+            }
+        },[utc])
+    return{
+
+        clock: state,
+    }
 };
 
 export default UseClock;
